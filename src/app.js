8a5e86a5ae8d4ab09dcd32d4a9bb963b5c5bf422
Agustin3z <agustin3z@live.com>
2026-04-08 15:35:14 -0300

    correcciones

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import mocksRouter from './routes/mocks.router.js';
+import YAML from 'yamljs';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const swaggerDocument = YAML.load('./src/docs/adoptme.yaml');
 
 // conexión a Mongo
 mongoose.connect(process.env.MONGO_URI)
@@ -27,5 +30,6 @@ app.use('/api/pets', petsRouter);
 app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mocksRouter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
